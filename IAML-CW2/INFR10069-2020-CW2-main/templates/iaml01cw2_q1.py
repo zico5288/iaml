8635f16b7069bcc9313875a6f7ca8933cb8db4e6
zico5288 <57965798+zico5288@users.noreply.github.com>
2021-06-17 23:05:30 +0800

    Add files via upload

---
+++ IAML-CW2/INFR10069-2020-CW2-main/templates/iaml01cw2_q1.py
@@ -0,0 +1,68 @@
+
+##########################################################
+#  Python script template for Question 1 (IAML Level 10)
+#  Note that
+#  - You should not change the name of this file, 'iaml01cw2_q1.py', which is the file name you should use when you submit your code for this question.
+#  - You should write code for the functions defined below. Do not change their names.
+#  - You can define function arguments (parameters) and returns (attributes) if necessary.
+#  - In case you define additional functions, do not define them here, but put them in a separate Python module file, "iaml01cw2_my_helpers.py", and import it in this script.
+#  - For those questions requiring you to show results in tables, your code does not need to present them in tables - just showing them with print() is fine.
+#  - You do not need to include this header in your submission.
+##########################################################
+
+#--- Code for loading modules and the data set and pre-processing --->
+# NB: You can edit the following and add code (e.g. code for loading sklearn) if necessary.
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import seaborn as sns
+from iaml01cw2_helpers import *
+# from iaml01cw2_my_helpers import *
+
+#<----
+
+# Q1.1
+def iaml01cw2_q1_1():
+#
+# iaml01cw2_q1_1()   # comment this out when you run the function
+
+# Q1.2
+def iaml01cw2_q1_2():
+#
+# iaml01cw2_q1_2()   # comment this out when you run the function
+
+# Q1.3
+def iaml01cw2_q1_3():
+#
+# iaml01cw2_q1_3()   # comment this out when you run the function
+
+
+# Q1.4
+def iaml01cw2_q1_4():
+#
+# iaml01cw2_q1_4()   # comment this out when you run the function
+
+
+# Q1.5
+def iaml01cw2_q1_5():
+#
+# iaml01cw2_q1_5()   # comment this out when you run the function
+
+
+# Q1.6
+def iaml01cw2_q1_6():
+#
+# iaml01cw2_q1_6()   # comment this out when you run the function
+
+
+# Q1.7
+def iaml01cw2_q1_7():
+#
+# iaml01cw2_q1_7()   # comment this out when you run the function
+
+
+# Q1.8
+def iaml01cw2_q1_8():
+#
+# iaml01cw2_q1_8()   # comment this out when you run the function
---
+++ IAML-CW2/INFR10069-2020-CW2-main/templates/iaml01cw2_q2.py
@@ -0,0 +1,54 @@
+
+##########################################################
+#  Python script template for Question 2 (IAML Level 10)
+#  Note that
+#  - You should not change the name of this file, 'iaml01cw2_q2.py', which is the file name you should use when you submit your code for this question.
+#  - You should write code for the functions defined below. Do not change their names.
+#  - You can define function arguments (parameters) and returns (attributes) if necessary.
+#  - In case you define additional functions, do not define them here, but put them in a separate Python module file, "iaml01cw2_my_helpers.py", and import it in this script.
+#  - For those questions requiring you to show results in tables, your code does not need to present them in tables - just showing them with print() is fine.
+#  - You do not need to include this header in your submission.
+##########################################################
+
+#--- Code for loading modules and the data set and pre-processing --->
+# NB: You can edit the following and add code (e.g. code for loading sklearn) if necessary.
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import seaborn as sns
+from iaml01cw2_helpers import *
+# from iaml01cw2_my_helpers import *
+
+#<----
+
+# Q2.1
+def iaml01cw2_q2_1():
+#
+# iaml01cw2_q2_1()   # comment this out when you run the function
+
+# Q2.2
+def iaml01cw2_q2_2():
+#
+# iaml01cw2_q2_2()   # comment this out when you run the function
+
+# Q2.3
+def iaml01cw2_q2_3():
+#
+# iaml01cw2_q2_3()   # comment this out when you run the function
+
+# Q2.4
+def iaml01cw2_q2_4():
+#
+# iaml01cw2_q2_4()   # comment this out when you run the function
+
+# Q2.5
+def iaml01cw2_q2_5():
+#
+# iaml01cw2_q2_5()   # comment this out when you run the function
+
+# Q2.6 
+def iaml01cw2_q2_6():
+#
+# iaml01cw2_q2_6()   # comment this out when you run the function
+
